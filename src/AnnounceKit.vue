dcdb177d56a338936dee8d555209ef5fd88d0838
Enrique Ramírez Vélez <hello@enrique-ramirez.com>
2024-08-27 20:00:17 -0600

    feat: Add aria-label attribute to script tag for better accessibility

--- src/AnnounceKit.vue
+++ src/AnnounceKit.vue
@@ -208,6 +208,7 @@ export default {
         var scripttag = document.createElement("script");
         scripttag["async"] = true;
         scripttag["src"] = `https://cdn.announcekit.app/widget-v2.js`;
+        scripttag.setAttribute("aria-label", "iframe");
 
         var scr = document.getElementsByTagName("script")[0];
         scr.parentNode.insertBefore(scripttag, scr);
